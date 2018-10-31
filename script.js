ce1aa403940e6158cf350efa750113650a0523ed
MoonlightSV <stalkervolosh@gmail.com>
2018-10-31 20:53:11 +0845

    Added all base files

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ function back(){
     carpet.style.top = '16px';
 }
 
-var login = document.getElementById('inp_login')
-var pass = document.getElementById('inp_pass')
+var login = document.getElementById('inp_login');
+var pass = document.getElementById('inp_pass');
 
 function sign_in(){
     if (login.value === "admin" && pass.value === "admin")
         document.location.href = "http://www.google.com";
+}
+
+function input(){
+    login.value = 'admin';
+    pass.value = 'admin';
 }
